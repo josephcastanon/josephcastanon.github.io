9fd093f7de6f838fd7907674fc09e6af241a8c82
josephcastanon <castj181@newschool.edu>
2017-05-11 18:32:48 -0400

    FINAL

---
+++ 30ARTISTS/collection.js
@@ -0,0 +1,203 @@
+var artists = [
+
+  // ARTIST 1 //
+  {
+    "Name": "CELERY",
+    "Location": "GREENWICH VILLAGE, NYC",
+    "img": "assets/images/Celery2.jpg",
+    "Link": "index18.html",
+    "None": "index18.html",
+  },
+
+
+  // ARTIST 2 //
+  {
+    "Name": "PHALLIC CUNT",
+    "Location": "GREENWICH VILLAGE, NYC",
+    "img": "assets/images/Phallic2.jpg",
+    "Link": "index2.html",
+  },
+
+
+  // ARTIST 3 //
+  {
+    "Name": "JO",
+    "Location": "EAST VILLAGE, NYC",
+    "img": "assets/images/JO2.jpg",
+    "Link": "index17.html",
+  },
+
+
+  // ARTIST 4 //
+  {
+    "Name": "PISSY PUSSY",
+    "Location": "BROOKLYN, NY",
+    "img": "assets/images/Pissy2.jpg",
+    "Link": "index3.html",
+  },
+
+
+    // ARTIST 5 //
+    {
+      "Name": "NAY CAMPBELL",
+      "Location": "CHELSEA, NY",
+      "img": "assets/images/Nay2.jpg",
+      "Link": "index16.html",
+    },
+
+
+    // ARTIST 6 //
+    {
+      "Name": "MALESTIA CHILD",
+      "Location": "BROOKLYN, NY",
+      "img": "assets/images/Malestia2.jpg",
+      "Link": "index5.html",
+    },
+
+
+    // ARTIST 7 //
+    {
+      "Name": "LUXURYCARCASS",
+      "Location": "BROOKLYN, NY",
+      "img": "assets/images/Lux2.jpg",
+      "Link": "index4.html",
+    },
+
+
+    // ARTIST 8 //
+    {
+      "Name": "ADA BANKS",
+      "Location": "BROOKLYN, NY",
+      "img": "assets/images/Ada2.jpg",
+      "Link": "index19.html",
+    },
+
+    // ARTIST 9 //
+    {
+      "Name": "PAULI CAKES",
+      "Location": "BROOKLYN, NY",
+      "img": "assets/images/Pauli2.jpg",
+      "Link": "index6.html",
+    },
+
+    // ARTIST 10 //
+    {
+      "Name": "LINUX",
+      "Location": "BROOKLYN, NY",
+      "img": "assets/images/Linux2.jpg",
+      "Link": "index8.html",
+    },
+
+    // ARTIST 11 //
+    {
+      "Name": "CHIMERA",
+      "Location": "GREENWICH VILLAGE, NYC",
+      "img": "assets/images/Chimera2.jpg",
+      "Link": "index7.html",
+    },
+
+    // ARTIST 12 //
+    {
+      "Name": "JOE!",
+      "Location": "BROOKLYN, NY",
+      "img": "assets/images/Joe2.jpg",
+      "Link": "index9.html",
+    },
+
+    // ARTIST 13 //
+    {
+      "Name": "KAYVON ZAND",
+      "Location": "MURRAY HILL, NYC",
+      "img": "assets/images/Kayvon2.jpg",
+      "Link": "index10.html",
+    },
+
+    // ARTIST 14 //
+    {
+      "Name": "MAGDALENA FEMANON",
+      "Location": "BROOKLYN, NY",
+      "img": "assets/images/Maggie2.jpg",
+      "Link": "index11.html",
+    },
+
+    // ARTIST 15 //
+    {
+      "Name": "TYGA TONIC",
+      "Location": "BROOKLYN, NY",
+      "img": "assets/images/Tyga2.jpg",
+      "Link": "index12.html",
+    },
+
+    // ARTIST 16 //
+    {
+      "Name": "JUPITER JADON",
+      "Location": "BROOKLYN, NY",
+      "img": "assets/images/Jupiter2.jpg",
+      "Link": "index13.html",
+    },
+
+    // ARTIST 18 //
+    {
+      "Name": "BRONWEN",
+      "Location": "GREENWICH VILLAGE, NYC",
+      "img": "assets/images/Bronwen2.jpg",
+      "Link": "index15.html",
+    },
+
+    // ARTIST 19 //
+    {
+      "Name": "MUFFY",
+      "Location": "WEST VILLAGE, NYC",
+      "img": "assets/images/Muffy2.jpg",
+      "Link": "index20.html",
+    },
+
+    // ARTIST 20 //
+    {
+      "Name": "BORDEN",
+      "Location": "EAST VILLAGE, NYC",
+      "img": "assets/images/Borden2.jpg",
+      "Link": "index21.html",
+    },
+
+    // ARTIST 21 //
+    {
+      "Name": "JUSTIN ANGEL",
+      "Location": "BROOKYLN, NY",
+      "img": "assets/images/Justin2.jpg",
+      "Link": "index22.html",
+    },
+
+    // ARTIST 22 //
+    {
+      "Name": "EGGZ BENEDICT",
+      "Location": "BROOKYLN, NY",
+      "img": "assets/images/Eggz2.jpg",
+      "Link": "index23.html",
+    },
+
+    // ARTIST 23 //
+    {
+      "Name": "JACOB HILL",
+      "Location": "EAST VILLAGE, NYC",
+      "img": "assets/images/Jacob2.jpg",
+      "Link": "index24.html",
+    },
+
+    // ARTIST 24 //
+    {
+      "Name": "JOSH",
+      "Location": "EAST VILLAGE, NYC",
+      "img": "assets/images/Josh2.jpg",
+      "Link": "index25.html",
+    },
+
+    // ARTIST 25 //
+    {
+      "Name": "HEATHER JONES",
+      "Location": "BRONX, NYC",
+      "img": "assets/images/Heather2.jpg",
+      "Link": "index26.html",
+    },
+
+];
